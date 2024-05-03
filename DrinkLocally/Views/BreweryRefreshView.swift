79f753276f952a9f657fbbc8483e7372cb735742
buzzamus <brent.busby@yahoo.com>
2024-05-03 08:48:36 -0500

    add Brewery refresh button view

---
+++ DrinkLocally/Views/BreweryRefreshView.swift
@@ -0,0 +1,44 @@
+//
+//  LocationRefreshView.swift
+//  DrinkLocally
+//
+//  Created by Brent Busby on 5/3/24.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct BreweryRefreshView: View {
+    let viewModel: BreweriesList
+    var body: some View {
+        ZStack {
+            Circle()
+                .foregroundColor(.brown)
+                .frame(width: 50, height: 50)
+            
+            Button(action: {
+                Task {
+                    do {
+                        try await refreshBreweries()
+                    } catch {
+                        print("Error refreshing breweries: \(error)")
+                    }
+                }
+            }) {
+                Image(systemName: "arrow.clockwise")
+                    .foregroundColor(.white)
+            }
+            .frame(width: 50, height: 50)
+        }
+        .padding(.bottom, 20) // Add padding to keep the button off the edge of the screen
+        .padding(.trailing, 20)
+    }
+    
+    private func refreshBreweries() async throws {
+        try await viewModel.populateBreweries()
+    }
+}
+
+#Preview {
+    BreweryRefreshView(viewModel: BreweriesList(locationManager: CLLocationManager()))
+}
